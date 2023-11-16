c0217f704a65143687cf54f6f13f15f28746c6e0
Nat M <nmac2000@mail.ubc.ca>
2023-11-16 15:19:33 -0800

    a good start

---
+++ b3/app.R
@@ -0,0 +1,39 @@
+
+
+library(shiny)
+library(tidyverse)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Iris"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("Species",
+                        "Iris Species",
+                        c("setosa","versicolor","virginica"))
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("irisPlot")
+          
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$irisPlot <- renderPlot({
+        iris1 <- filter(iris, Species == input$Species)
+        ggplot(iris1, aes(x=Petal.Length, y=Sepal.Width)) +
+                 geom_point(color="blue", position = "jitter")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)

f83925ff9585dcef75f07d60d9385d38a48f9eb7
Nat M <nmac2000@mail.ubc.ca>
2023-11-22 14:14:51 -0800

    published

--- b3/app.R
+++ b3/app.R
@@ -9,23 +9,23 @@ ui <- fluidPage(
     # Application title
     titlePanel("Iris"),
 
-    # Sidebar with a slider input for number of bins 
+    #Sidebar with options to choose species for the graph, which is useful because it allows the user to see the differneces between each species
         fluidRow(
-        column(4,
+        column(4, 
             selectInput("Species",
                         "Iris Species",
                         c("setosa","versicolor","virginica"))
         ),
 
-        # Show a plot of the generated distribution
+        # Show a plot of the petal and sepal length
         column(8,
           plotOutput("irisPlot")
           
         )
     ),
     fluidRow(
-      column(12,
-             img(src='iris.png', align = "right")
+      column(12, #Had to use fluidRow and make a unique layout with columns so that I could put the image on the same page as the graph
+             img(src='iris.png', align = "right") #add image showing the 3 different species, which is useful because it shows what the petal and sepal looks like on each flower
     ))
 )
 
@@ -34,7 +34,7 @@ server <- function(input, output) {
 
   output$irisPlot <- renderPlot({
       iris1 <- filter(iris, Species == input$Species)
-      ggplot(iris1, aes(x=Petal.Length, y=Sepal.Width)) +
+      ggplot(iris1, aes(x=Petal.Length, y=Sepal.Length)) +
       geom_point(color="deepskyblue4", position = "jitter") 
                   
     })
